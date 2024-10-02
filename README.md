bf3d0d477a54c19d4fd9fff68d1f2c9382f308fc
Tobey Blaber <contact@tobey.me>
2024-10-02 14:19:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Tobey's Timestamp Logger for BepInEx
+
+A configurable BepInEx patcher to log the current timestamp.
+
+## Why?
+
+When troubleshooting, it's useful to know the time and date of the log file you're looking at. Contrary to popular belief, BepInEx does not log this by default - the timestamp at the top of the log file is the timestamp of the modified date of the game's executable, which is still useful but for different reasons.
+
+Sometimes, users will be unaware that the log file they're reading is irrelevant because it is from a previous run of the game prior to making changes. Additionally, when users share their log files with helpful folks donating their time to help them out, they have no way of knowing the log file's timestamp, which can often lead to wasted time for all involved.
+
+To address this, Tobey.BepInEx.Timestamp prints a UTC timestamp in the logs to make it easier for users and helpers alike to determine whether the log file they're looking at is relevant.
+
+By default, it does this by contacting a remote endpoint (http://google.com) and parsing the time and date from the response headers, falling back to the system clock if something goes wrong.
+
+### Why contact a remote endpoint for the timestamp? Every computer has a system clock, so why not just use it?
+
+In a perfect world we could depend upon the validity of the system clock, however sometimes users have system clocks which are incorrectly set for a variety of reasons. For example, some users manually change their system clock to artificially extend the length of free software trials. My opinion is that a false timestamp in the logs is more harmful than no timestamp at all, potentially leading to wasted time for volunteer helpers.
+
+If for whatever reason you don't want your computer reaching out to http://google.com when you load the game, you can edit the configuration to either change the remote endpoint used, or altenatively disable remote timestamp acquisition altogether.
+
+## Usage
+
+Just plop the contents of the downloaded .zip from [the releases page](https://github.com/toebeann/Tobey.MLLoader.GameInfo/releases) into your game folder (after installing [BepInEx](https://github.com/BepInEx/BepInEx), of course).
+
+If you would like to configure the behaviour of remote timestamp acquisition, you can edit the file `BepInEx` > `config` > `Tobey.BepInEx.Timestamp.cfg`:
+
+```cfg
+[Remote]
+
+## Allow acquiring timestamp from remote endpoints for better accuracy
+# Setting type: Boolean
+# Default value: true
+Enabled = true
+
+## Endpoint URI for remote timestamp acquisition, which will be parsed from the response headers
+## The endpoint's response must contain a "date" header in the format: ddd, dd MMM yyyy HH:mm:ss GMT
+## Example: Wed, 02 Oct 2024 12:09:25 GMT
+## HTTPS is not supported
+# Setting type: String
+# Default value: http://google.com
+Endpoint = http://google.com
+```
---
+++ Tobey.BepInEx.Timestamp/Patcher.cs
@@ -0,0 +1,89 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace Tobey.BepInEx.Timestamp;
+
+public static class Patcher
+{
+    // Without the contents of this region, the patcher will not be loaded by BepInEx - do not remove!
+    #region BepInEx Patcher Contract
+    public static IEnumerable<string> TargetDLLs { get; } = [];
+    public static void Patch(AssemblyDefinition _) { }
+    #endregion
+
+    // entry point - do not delete or rename!
+    public static void Initialize()
+    {
+        using var logger = Logger.CreateLogSource("Timestamp");
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        var source = "local system clock";
+
+        ConfigFile config = new(
+            configPath: Path.Combine(Paths.ConfigPath, "Tobey.BepInEx.Timestamp.cfg"),
+            saveOnInit: true);
+
+        var remoteEnabled = config.Bind(
+            section: "Remote",
+            key: "Enabled",
+            defaultValue: true,
+            description: "Allow acquiring timestamp from remote endpoints for better accuracy");
+
+        var remoteEndpoint = config.Bind(
+            section: "Remote",
+            key: "Endpoint",
+            defaultValue: "http://google.com",
+            description: string.Join(Environment.NewLine,
+                [
+                    "Endpoint URI for remote timestamp acquisition, which will be parsed from the response headers",
+                    "The endpoint's response must contain a \"date\" header in the format: ddd, dd MMM yyyy HH:mm:ss GMT",
+                    "Example: Wed, 02 Oct 2024 12:09:25 GMT",
+                    "HTTPS is not supported"
+                ]));
+
+        if (remoteEnabled.Value)
+        {
+            string[] sources = [remoteEndpoint.Value, (string)remoteEndpoint.DefaultValue];
+            foreach (var endpoint in sources.Distinct())
+            {
+                try
+                {
+                    now = GetRemoteTimestamp(endpoint);
+                    source = endpoint;
+                    break;
+                }
+                catch
+                {
+                    logger.LogWarning($"Failed to get remote timestamp from {endpoint}");
+                }
+            }
+        }
+        else
+        {
+            logger.LogInfo("Remote timestamp acquisition disabled");
+        }
+
+        logger.LogMessage($"It is currently {now:R} according to {source}");
+        Logger.Sources.Remove(logger);
+    }
+
+    private static DateTimeOffset GetRemoteTimestamp(string sourceUriString)
+    {
+        var request = (HttpWebRequest)WebRequest.Create(sourceUriString);
+        request.Timeout = 1000;
+        using var response = request.GetResponse();
+
+        return DateTimeOffset.ParseExact(
+            input: response.Headers["date"],
+            format: "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+            CultureInfo.InvariantCulture.DateTimeFormat,
+            DateTimeStyles.AssumeUniversal);
+    }
+}
